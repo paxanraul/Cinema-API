47964231979ae4b5cf4f04998881f04e942cb920
paxanraul <raul20081033@gmail.com>
2026-06-01 17:51:27 +0300

    add schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserRegister(BaseModel):
+    first_name: str = Field(..., description="Enter your first name.")
+    second_name: str = Field(..., description="Enter your second name.")
+    patronymic: str | None = Field(..., description="Enter your patronymic.")
+    email: EmailStr = Field(..., description="Enter your email.")
+    password: str = Field(..., min_length=8, description="Enter your password.")
+    password_confirm: str = Field(..., min_length=8, description="Confirm your password.")
+
+
+class UserLogin(UserBase):
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    first_name: str
+    second_name: str
+    patronymic: str | None = None
+    email: EmailStr
+    is_active: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
