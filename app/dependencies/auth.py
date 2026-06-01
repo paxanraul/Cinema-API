7c308c1dacd7ea796763c83efc7f1901b1301b47
paxanraul <raul20081033@gmail.com>
2026-06-01 20:23:25 +0300

    add dependency auth.py

---
+++ app/dependencies/auth.py
@@ -0,0 +1,48 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+import jwt
+
+from app.db.session import get_db
+from app.core.config import settings
+from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db)
+):
+    # Verifies the PyJWT token and performs all user checks in the database
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        sub: str = payload.get("sub")
+
+        if sub is None:
+            raise jwt.InvalidTokenError
+        
+        user_id = int(sub)
+
+    except (jwt.PyJWTError, ValueError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Autheticate": "Bearer"},
+        )
+    
+    # Verifying the user's existence in the database
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    
+    # Checking the activity
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user account"
+        )
+    
+    return user
