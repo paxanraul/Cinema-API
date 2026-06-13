9928e25ebc76585c053170db4442e84684c0c001
paxanraul <raul20081033@gmail.com>
2026-06-13 15:54:35 +0300

    add: working launch of the backend

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from app.routers import auth
+from app.routers import auth, sessions, bookings, movies
+from app.db.session import SessionLocal
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	db = SessionLocal()
+	try:
+		yield
+	finally:
+		db.close()		
 
 
 app = FastAPI(
     title="CinemaAPI",
     description="Backend API for cinema service",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan=lifespan
 )
 
 
 app.include_router(auth.router)
+app.include_router(sessions.router)
+app.include_router(bookings.router)
+app.include_router(movies.router)
+
+@app.get("/health")
+def get_health():
+	return {"status": "ok"}
