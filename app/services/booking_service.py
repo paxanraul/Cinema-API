c9cd9941bb5fb86472a789c480b0dcf025523d7c
paxanraul <raul20081033@gmail.com>
2026-06-15 19:21:45 +0300

    add: a little bit business logic

--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -1,8 +1,20 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
+from datetime import datetime, timezone
+
 from app.models.booking import Booking
 from app.schemas.booking import BookingCreate
+from app.models.session import Session as SessionModel
 
 def create_booking(db: Session, booking_data: BookingCreate, user_id: int) -> Booking:
+	session = db.query(SessionModel).filter(SessionModel.id == booking_data.session_id).first()
+	
+	if not session:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Сеанс не найден")
+
+	if session.start_time < datetime.now(timezone.utc):
+		raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Сеанс уже прошёл")
+
 	booking = Booking(**booking_data.model_dump(), user_id=user_id)
 	db.add(booking)
 	db.commit()
