5245af7b272d746e385a6239077bd33110847662
paxanraul <raul20081033@gmail.com>
2026-06-19 16:29:29 +0300

    add: test_movies

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -16,3 +16,26 @@ def test_get_movies_authorized(client, user_data):
 	response = client.get("/movies/all_movies", headers={"Authorization": f"Bearer {token}"})
 	assert response.status_code == 200
 	assert isinstance(response.json(), list)
+
+
+def test_create_movie(client, user_data):
+	client.post("/auth/register", json=user_data)
+	login = client.post("/auth/login", json={
+		"email": "test@test.com",
+		"password": "password123"
+	}) 
+	token = login.json()["access_token"]
+
+	response = client.post("/movies/create_movie",
+		json={
+			"name": "Тест",
+			"year": 2026,
+			"description": "Описание",
+			"duration_limites": 120,
+			"genre": "Crime",
+			"director": "Иванов"
+		},
+		headers={"Authorization": f"Bearer {token}"}
+	)
+	print(response.json())
+	assert response.status_code == 201
