565fa29fb86b395ca5736a90f63914e0a0f6608a
paxanraul <raul20081033@gmail.com>
2026-06-20 14:33:26 +0300

    delete: extra import

--- app/routers/movies.py
+++ app/routers/movies.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
-from app.models.movie import Movie
 from app.models.user import User
 from app.dependencies.auth import get_current_user
 from app.schemas.movie import MovieCreate, MovieResponse
