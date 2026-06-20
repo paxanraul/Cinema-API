d66a174d94c54acb76404dd20e697f558bf6eeeb
paxanraul <raul20081033@gmail.com>
2026-06-20 14:27:46 +0300

    add: is_active in model

--- app/models/hall.py
+++ app/models/hall.py
@@ -11,5 +11,6 @@ class Hall(Base):
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     capacity: Mapped[int] = mapped_column(nullable=False)
     location: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
 
     sessions: Mapped[list["Session"]] = relationship(back_populates="hall") # type: ignore
